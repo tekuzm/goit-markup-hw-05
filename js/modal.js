17f68f5e9f3a9f282e1f952aa5062357c2baba59
TetianaKuzma <tetiana.b97@gmail.com>
2022-08-09 16:28:12 +0300

    Update image

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+    const { height: pageHeaderHeight } = document
+		.querySelector(".page-header")
+		.getBoundingClientRect();
+
+    document.body.style.paddingTop = `${pageHeaderHeight}px`;
+})();
